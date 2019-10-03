1aae0a65fda5100c660d007bdb57747f7d6f98b7
Panagiotis Argyrakis <panargirakis@gmail.com>
2019-10-02 21:03:34 -0500

    Cleanup

--- empty-project-2/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Collaborator is " + "Jonathan");
-        System.out.println("Collaborators are  " + "Henry and Ryan");
-        System.out.println("Panos");
-        System.out.println("Dimitri Berardi");
-    }
-}
